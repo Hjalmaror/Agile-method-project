2b4920e0cab1ea31217f01ebeb72990374c762a8
inaschmitz <ina.e.schmitz@gmail.com>
2015-03-18 10:53:02 +0100

    forsätter fixa front-page

--- front-page.php
+++ front-page.php
@@ -12,12 +12,30 @@ get_header();
 
 ?>
 
-<div id="container">
+<section class="section1">
 	<div id="code">
 		<h1>Crack the code</h1>
-		<input type="text" />
+		<input type="text" autofocus />
 	</div>
-</div>
+</section>
+
+<section class="section2">
+	<div class="contest-info">
+		<h1>Tävlingsregler</h1>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</section>
+
+<section class="section3">
+	<div class="news">
+		<h1>Nyhetsflöde</h1>
+	</div>
+</section>
 
 <?php
 get_footer();
